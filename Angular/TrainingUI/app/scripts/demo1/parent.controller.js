2442b5c541078219acf6239341ee4385438b2c22
KhoiVu <tuankhoi2206@gmail.com>
2018-09-20 21:11:22 +0700

    Descript: Update Angular Project Pushed by: Khoi Vu

---
+++ Angular/TrainingUI/app/scripts/demo1/parent.controller.js
@@ -0,0 +1,7 @@
+/**
+ * Created by hoang yen on 13/04/2017.
+ */
+angular.module('myApp', []).controller('ParentController', ['$scope', function ($scope) {
+  $scope.name = "dj";
+  $scope.age = 32;
+}]);
